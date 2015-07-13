91871ffc38594f931fcf5a8af6a3d971bc1a4c21
JimCampagno <jimcampagno@gmail.com>
2015-07-13 11:13:02 -0400

    add apiclient

---
+++ locationTrivia-API/FISAPIClient.h
@@ -0,0 +1,13 @@
+//
+//  FISAPIClient.h
+//  locationTrivia-API
+//
+//  Created by James Campagno on 7/13/15.
+//  Copyright (c) 2015 Joe Burgess. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FISAPIClient : NSObject
+
+@end
---
+++ locationTrivia-API/FISAPIClient.m
@@ -0,0 +1,17 @@
+//
+//  FISAPIClient.m
+//  locationTrivia-API
+//
+//  Created by James Campagno on 7/13/15.
+//  Copyright (c) 2015 Joe Burgess. All rights reserved.
+//
+
+#import "FISAPIClient.h"
+#import <AFNetworking/AFHTTPRequestOperationManager.h>
+
+#define API_KEY @"8efafff01e66130686fb5a76ac9f99abfb55103a"
+#define BASE_URL @"http://locationtrivia.herokuapp.com/"
+
+@implementation FISAPIClient
+
+@end
